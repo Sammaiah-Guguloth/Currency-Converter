5fa721f142c2fb3c5d31a7004996426fc74001c4
Sammaiah-Guguloth <gugulothsammaiah5@gmail.com>
2024-04-09 18:53:48 +0530

    added all the files

---
+++ README.md
@@ -0,0 +1,33 @@
+# Currency Converter Website
+
+Welcome to the Currency Converter website! This website was built as a practice project to enhance skills in HTML, CSS, and JavaScript. With this currency converter, you can easily convert between different currencies and stay updated with the latest exchange rates.
+
+## Features
+
+- **Simple Interface**: The website has a clean and intuitive user interface, making it easy for users to navigate and use the currency converter.
+- **Real-time Exchange Rates**: It utilizes real-time exchange rate data to ensure accurate conversions.
+- **Multiple Currency Support**: You can convert between a wide range of currencies, making it versatile for various needs.
+- **Responsive Design**: The website is designed to be responsive, ensuring it looks great and functions well on different devices and screen sizes.
+
+## Technologies Used
+
+- **HTML**: Used for structuring the content of the website.
+- **CSS**: Used for styling the website and making it visually appealing.
+- **JavaScript**: Used for implementing the currency conversion functionality and fetching real-time exchange rate data.
+
+## How to Use
+
+1. **Enter Amount**: Enter the amount of money you want to convert.
+2. **Select Currencies**: Choose the currency you want to convert from and the currency you want to convert to from the dropdown menus.
+3. **Convert**: Click on the "Convert" button to see the converted amount.
+4. **Real-time Rates**: The exchange rates are automatically fetched and updated in real-time, ensuring accuracy.
+
+## GITHUB-Pages
+
+
+## Credits
+
+This project was created by [Sammaiah Guguloth] as a practice project for learning HTML, CSS, and JavaScript, following the tutorial by Shraddha Khapra. You can find the tutorial on [YouTube](https://youtu.be/CyGodpqcid4?si=hPwAdYu7XBLx6N5W).
+
+
+
---
+++ index.js
@@ -0,0 +1,74 @@
+// let imgUrl = "https://flagsapi.com/US/flat/64.png"
+const CURR_URL = "https://v6.exchangerate-api.com/v6/5840a1504758a7f7681f5568/latest" // and base currency
+
+
+const dropDowns = document.querySelectorAll('select');
+const convertBtn = document.querySelector('#convert-btn');
+
+
+for(let select of dropDowns) {
+    for(let country in countryList) {
+        let newOpt = document.createElement('option');
+        if(select.name === "to" && country === "INR") {
+            newOpt.selected = "selected";
+        }
+        if(select.name === "from" && country === "USD") {
+            newOpt.selected = "selected";
+        }
+        newOpt.value = country;
+        newOpt.innerText = countryList[country]
+
+        select.append(newOpt);
+
+        select.addEventListener('change' , (event) => {
+            let countryCode = event.target.value;
+            let currCountry = countryCode.substring(0 , countryCode.length-1);
+            
+            let countryImg = select.parentElement.querySelector("img");
+            countryImg.src = `https://flagsapi.com/${currCountry}/flat/64.png`;
+        })
+    }
+}
+
+async function calcCurrency() {
+    let fromCurr = document.querySelector('#from').value;
+    let toCurr = document.querySelector("#to").value;
+    let amount = document.querySelector('#amount-inp').value;
+
+    const response = await fetch(`${CURR_URL}/${fromCurr}`);
+    const data = await response.json();
+
+    const conversionRates = data.conversion_rates
+    let totalAmount = conversionRates[toCurr] * amount;
+    totalAmount = totalAmount.toFixed(3);
+    
+    display(fromCurr , toCurr , amount , totalAmount );
+}
+
+function display(fromCurr , toCurr ,amount , totalAmount) {
+    let displayBox = document.querySelector('.info-box p');
+    let result = `${amount} ${fromCurr}  =  ${totalAmount} ${toCurr}`;
+    displayBox.innerText = result;
+}
+
+convertBtn.addEventListener("click" , (event) => {
+    event.preventDefault();
+    calcCurrency();
+});
+
+
+// additional functionalites
+let inputBox = document.querySelector('#amount-inp');
+inputBox.addEventListener('input' , () => {
+    let value = inputBox.value;
+    
+    let sanitisedValue = '';
+    for(let i=0; i<value.length; i++) {
+        if(!isNaN(value[i])) {
+            sanitisedValue += value[i];
+        }
+    }
+
+    inputBox.value = sanitisedValue;
+})
+
